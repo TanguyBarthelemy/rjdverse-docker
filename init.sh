0822e0d4a0db7197bc85be4f11293fa53ad888a6
Tanguy Barthelemy <tanguy.barthelemy@insee.fr>
2025-10-09 00:03:01 +0200

    add air

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 echo "
 .libPaths(file.path(Sys.getenv(\"HOME\"), \"renv\", \"library\"))
 " >> "${HOME}/.Rprofile"
+
+curl -LsSf https://github.com/posit-dev/air/releases/latest/download/air-installer.sh | sh
+source $HOME/.local/bin/env
